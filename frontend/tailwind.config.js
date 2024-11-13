d6f9e0cc17d0aea768cf7650852e9dcf2d4aa6ff
krish4uu <prajapatikashyap97@gmail.com>
2024-11-12 22:56:53 -0500

    Frontend setup files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violet: {
+          600: '#7C3AED',
+          700: '#6D28D9',
+        }
+      },
+    },
+  },
+}
